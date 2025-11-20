607f306248f522e094f70676969808e68bda256f
Hujim0 <Hujim0@users.noreply.github.com>
2025-11-20 14:46:20 +0300

    refactor add-space-marine to create-space-marine && add space-marine-selector

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    reactCompiler: true,
-    output: "export",
-    distDir: "out",
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: "http://localhost:8080/api/:path*",
-            },
-        ];
-    },
+  /* config options here */
+  reactCompiler: true,
+  output: 'export',
+  distDir: 'out',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- frontend/src/components/main-content.tsx
+++ frontend/src/components/main-content.tsx
@@ -1,26 +1,29 @@
 "use client"
 
 import { useState } from "react";
-import { SpaceMarinesTable } from "./space-marines-table";
-import { AddSpaceMarineDialogContent } from "./add-space-marine-dialog";
-import { Dialog } from "@radix-ui/react-dialog";
-import { DialogTrigger } from "./ui/dialog";
+import { SpaceMarinesTable } from "./space-marine/space-marines-table";
+import { CreateSpaceMarineDialogContent } from "./space-marine/create-space-marine-dialog";
 import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
 
 export function MainContent() {
     const [open, setOpen] = useState(false);
 
     return <div className="flex flex-col justify-center gap-2">
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button>asd</Button>
-            </DialogTrigger>
-            <AddSpaceMarineDialogContent setOpen={setOpen}></AddSpaceMarineDialogContent>
-
-        </Dialog>
+        <Button onClick={(e) => {
+            e.stopPropagation();
+            setOpen(true);
+        }}>
+            <Plus className="mr-2 w-4 h-4" /> Create New
+        </Button>
 
         <SpaceMarinesTable>
         </SpaceMarinesTable>
+
+        <CreateSpaceMarineDialogContent
+            open={open}
+            onOpenChange={setOpen}
+        />
     </div>
 
 }
